51bc651584ea704d3f4ec267252e64136437389a
Fel John <feljohn.loe.bangasan@gmail.com>
2022-06-30 23:43:58 +0800

    reset

--- products/models.py
+++ products/models.py
@@ -10,15 +10,15 @@ class Product(models.Model):
     product_name        = models.CharField(max_length=125)
     price_per_piece     = models.DecimalField(max_digits=10, decimal_places=2) # Cost Price
     retail_per_piece    = models.DecimalField(max_digits=10, decimal_places=2) # Retail Price
-    product_category    = models.CharField(max_length=50) #Category Id
     inventory_received  = models.IntegerField(default=0, blank=True, null=True)
     inventory_shipped   = models.IntegerField(default=0, blank=True, null=True)
     inventory_on_hand   = models.IntegerField(default=0, blank=True, null=True)
     minimum_required    = models.IntegerField(default=0, blank=True, null=True)
 
-    supplier            = models.ForeignKey(Supplier , null=True ,on_delete=models.CASCADE)
-    created_at          = models.DateField(default = datetime.now)
-    updated_at          = models.DateField()
+    product_category    = models.ForeignKey('Category' , null=True ,on_delete = models.SET_NULL)
+    supplier            = models.ForeignKey(Supplier , null=True ,on_delete = models.SET_NULL)
+    created_at          = models.DateTimeField(default = datetime.now)
+    updated_at          = models.DateTimeField()
 
 
 class Category(models.Model):
